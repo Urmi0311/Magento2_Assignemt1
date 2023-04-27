065d6c7ee336152de0819a2081abc799a42d6fa0
Urmi0311 <urmi.kothari@sigmainfo.net>
2023-04-27 16:48:51 +0530

    plase3/plugin completed

--- app/code/CUSTOM/ProductPlugins/Plugin/ProductBPlugin.php
+++ app/code/CUSTOM/ProductPlugins/Plugin/ProductBPlugin.php
@@ -1,7 +1,38 @@
 <?php
+
 namespace CUSTOM\ProductPlugins\Plugin;
+
+use Magento\Catalog\Model\Product;
 
 class ProductBPlugin
 {
+    public function beforeGetName(Product $subject)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/plugin-sortorder.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('Before Execute From Plugin B');
+    }
+    public function aroundGetName(Product $subject,  callable $proceed)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/plugin-sortorder.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('Around Before Proceed Execute From Plugin B');
 
+        $return = $proceed();
+
+        $logger->info('Around After Proceed Execute From Plugin B');
+
+        return $return;
+    }
+
+    public function afterGetName(Product $subject, $result)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/plugin-sortorder.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('After Execute From Plugin B');
+        return $result;
+    }
 }
--- app/code/CUSTOM/ProductPlugins/Plugin/ProductCPlugin.php
+++ app/code/CUSTOM/ProductPlugins/Plugin/ProductCPlugin.php
@@ -1,7 +1,38 @@
 <?php
+
 namespace CUSTOM\ProductPlugins\Plugin;
+
+use Magento\Catalog\Model\Product;
 
 class ProductCPlugin
 {
+    public function beforeGetName(Product $subject)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/plugin-sortorder.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('Before Execute From Plugin C');
+    }
+    public function aroundGetName(Product $subject,  callable $proceed)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/plugin-sortorder.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('Around Before Proceed Execute From Plugin C');
 
+        $return = $proceed();
+
+        $logger->info('Around After Proceed Execute From Plugin C');
+
+        return $return;
+    }
+
+    public function afterGetName(Product $subject, $result)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/plugin-sortorder.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('After Execute From Plugin C');
+        return $result;
+    }
 }
